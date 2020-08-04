1de965724006b4a236e92fd7b2765cb270172364
DataSecs <datasecs@outlook.de>
2020-08-04 17:50:46 +0200

    Starts working on command processing of server. Also adds lib for hash table.

---
+++ src/main.c
@@ -0,0 +1,48 @@
+#include "globals.h"
+#include "network/server.h"
+#include "map/map.h"
+
+// Main
+int main(int argc, char* argv[]) {
+    char* mapName = NULL;
+    char* hostAddress = "127.0.0.1";
+    int port = 8080;
+    double timeLimit = -1;
+    int depthLimit = -1;
+
+    for (int i = 0; i < argc; ++i) {
+        if (strcmp(argv[i], "-map") == 0 || strcmp(argv[i], "--map") == 0) {
+            mapName = argv[i + 1];
+        } else if (strcmp(argv[i], "-host") == 0 || strcmp(argv[i], "--host") == 0) {
+            hostAddress = argv[i + 1];
+        } else if (strcmp(argv[i], "-port") == 0 || strcmp(argv[i], "--port") == 0) {
+            port = (int) strtol(argv[i + 1], NULL, 10);
+        } else if (strcmp(argv[i], "-timeLimit") == 0 || strcmp(argv[i], "--timeLimit") == 0) {
+            timeLimit = (int) strtod(argv[i + 1], NULL);
+        } else if (strcmp(argv[i], "-depthLimit") == 0 || strcmp(argv[i], "--depthLimit") == 0) {
+            depthLimit = (int) strtol(argv[i + 1], NULL, 10);
+        }
+    }
+
+    // Check validity of input
+    if (mapName == NULL) {
+        fprintf(stderr, RED "mapName is required!" RESET "\n");
+        exit(1);
+    }
+
+    if (timeLimit == -1 && depthLimit == -1) {
+        fprintf(stderr, RED "timeLimit or depthLimit is required!" RESET "\n");
+        exit(1);
+    } else if (timeLimit < 0.2) {
+        fprintf(stderr, RED "timeLimit has to be >= 0.2!" RESET "\n");
+        exit(1);
+    }
+
+    // Print server settings
+
+
+    // On start-up the server waits for the required number of players to join the server
+    startServer(hostAddress, port);
+
+    return 0;
+}
