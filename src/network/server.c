b3c4252f3e050cc81063b91df23ad2ece780062d
DataSecs <datasecs@outlook.de>
2020-08-08 21:58:26 +0200

    Starts working on network communication. Basic socket communication is working.

--- src/network/server.c
+++ src/network/server.c
@@ -1,11 +1,30 @@
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "hicpp-signed-bitwise"
 //
 // Created with <3 by DataSecs on 03.08.20.
 //
 
 #include "server.h"
+#include "../map/map.h"
 
-// Get sockaddr, IPv4 or IPv6:
+
+
+//// Variables
+//////////////
+int fdCount;
+struct pollfd* pollFDs;
+char* host;
+char* port;
+int connectionLimit;
+int timeLimit;
+int depthLimit;
+
+
+
+//// Private funtions
+/////////////////////
 void* getSocketAddress(struct sockaddr* socketAddress) {
+    // Get socket adress in either IPv4 or IPv6
     if (socketAddress->sa_family == AF_INET) {
         return &(((struct sockaddr_in*) socketAddress)->sin_addr);
     } else {
@@ -13,13 +32,11 @@ void* getSocketAddress(struct sockaddr* socketAddress) {
     }
 }
 
-// Return a listener socket
-int getListenerSocket(void) {
+int getListenerSocket(int backlog) {
     // Listening socket descriptor
     int listener;
-    // For setsockopt() SO_REUSEADDR, below
-    int yes = 1;
-    int rv;
+    // For setsockopt() SO_REUSEADDR
+    int reUseAddresses = 1;
 
     /*
      * struct addrinfo {
@@ -37,24 +54,26 @@ int getListenerSocket(void) {
     struct addrinfo hints;
     struct addrinfo* addressList;
 
-    // Get us a socket and bind it
     memset(&hints, 0, sizeof hints);
     hints.ai_family = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-    if ((rv = getaddrinfo(NULL, PORT, &hints, &addressList)) != 0) {
-        fprintf(stderr, "selectserver: %s\n", gai_strerror(rv));
+    //hints.ai_flags = AI_PASSIVE;
+
+    int result = getaddrinfo(host, port, &hints, &addressList);
+    if (result != 0) {
+        fprintf(stderr, "selectserver: %s\n", gai_strerror(result));
         exit(1);
     }
 
-    for (struct addrinfo* address = addressList; address != NULL; address = address->ai_next) {
+    struct addrinfo* address;
+    for (address = addressList; address != NULL; address = address->ai_next) {
         listener = socket(address->ai_family, address->ai_socktype, address->ai_protocol);
         if (listener < 0) {
             continue;
         }
 
         // Lose the pesky "address already in use" error message
-        setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int));
+        setsockopt(listener, SOL_SOCKET, SO_REUSEADDR, &reUseAddresses, sizeof(int));
 
         if (bind(listener, address->ai_addr, address->ai_addrlen) < 0) {
             close(listener);
@@ -65,41 +84,157 @@ int getListenerSocket(void) {
     }
 
     // If we got here, it means we didn't get bound
-    if (p == NULL) {
+    if (address == NULL) {
         return -1;
     }
 
-    freeaddrinfo(addressList); // All done with this
+    freeaddrinfo(addressList);
 
     // Listen
-    if (listen(listener, 10) == -1) {
+    if (listen(listener, backlog) == -1) {
         return -1;
     }
+
+    printf("Server listening on " CYAN "%s" RESET ":" CYAN "%s\n" RESET, host, port);
+    fflush(stdout);
 
     return listener;
 }
 
-// Add a new file descriptor to the set
-void addFD(struct pollfd** pollFDs, int newFD, int* fdCount) {
-    (*pollFDs)[*fdCount].fd = newFD;
-    (*pollFDs)[*fdCount].events = POLLIN;
-    (*fdCount)++;
+void addFD(int newFD) {
+    pollFDs[fdCount].fd = newFD;
+    pollFDs[fdCount].events = POLLIN;
+    fdCount++;
 }
 
-// Remove an index from the set
-void del_from_pfds(struct pollfd pfds[], int i, int *fd_count)
-{
-    // Copy the one from the end over this one
-    pfds[i] = pfds[*fd_count-1];
+void removeFDInConnectionPhase(int playerNumber) {
+    // Copy the one from the end to the deleted one
+    pollFDs[playerNumber] = pollFDs[fdCount - 1];
 
-    (*fd_count)--;
+    fdCount--;
 }
 
-void sendMap() {
-
+void removeFDInGamePhase(int playerNumber) {
+    // Closes connection with player
+    close(pollFDs[playerNumber].fd);
+    pollFDs[playerNumber].fd = -1;
 }
 
-void startServer(char* bindAddress, int port) {
+void sendMessage(void* message, size_t msgLength, char* errorType, int playerNumber) {
+    if (send(pollFDs[playerNumber].fd, message, msgLength, 0) == -1) {
+        printf(RED "Error while sending " BOLDRED "%s" RESET RED " to player " BLUE "%i!\n" RESET, errorType, playerNumber);
+        printf(RED "Removing player " BLUE "%i" RED " from match!\n" RESET, playerNumber);
+        fflush(stdout);
+        close(pollFDs[playerNumber].fd);
+        removeFDInGamePhase(playerNumber);
+    }
+}
+
+void sendMoveRequest(int playerNumber) {
+    // Send message type
+    int8_t messageType = 4;
+    sendMessage(&messageType, 1, "message type", playerNumber);
+
+    // Send length
+    int32_t messageSize = htonl(5);
+    sendMessage(&messageSize, 4, "message size", playerNumber);
+
+    // Send time limit
+    int32_t messageTimeLimit = htonl(timeLimit);
+    sendMessage(&messageTimeLimit, 4, "time limit", playerNumber);
+
+    // Send depth limit
+    int8_t messageDepthLimit = depthLimit;
+    sendMessage(&messageDepthLimit, 1, "depth limit", playerNumber);
+}
+
+void sendDisqualification(int playerNumber) {
+    removeFDInGamePhase(playerNumber);
+
+    for (int i = 1; i <= fdCount; ++i) {
+        if (pollFDs[i].fd == -1) {
+            continue;
+        }
+
+        // Send message type
+        int8_t messageType = 7;
+        sendMessage(&messageType, 1, "message type", playerNumber);
+
+        // Send length
+        int32_t length = htonl(1);
+        sendMessage(&length, 4, "message size", playerNumber);
+
+        // Send length
+        int8_t player = playerNumber;
+        sendMessage(&player, 1, "player disqualification", playerNumber);
+    }
+}
+
+void sendMoveAnnoucement(Coordinate* move, int specialField) {
+    for (int playerNumber = 1; playerNumber <= fdCount; ++playerNumber) {
+        if (pollFDs[playerNumber].fd == -1) {
+            continue;
+        }
+
+        // Send message type
+        int8_t messageType = 4;
+        sendMessage(&messageType, 1, "message type", playerNumber);
+
+        // Send length
+        int32_t length = htonl(5);
+        sendMessage(&length, 4, "message size", playerNumber);
+
+        // Send x coordinate
+        int16_t x = htons(move->x);
+        sendMessage(&x, 2, "move x coordinate", playerNumber);
+
+        // Send x coordinate
+        int16_t y = htons(move->y);
+        sendMessage(&y, 2, "move y coordinate", playerNumber);
+
+        // Send x coordinate
+        int8_t choice = specialField;
+        sendMessage(&choice, 1, "special field", playerNumber);
+    }
+}
+
+void sendPhaseAnnoucement(int phase) {
+    for (int playerNumber = 1; playerNumber <= fdCount; ++playerNumber) {
+        if (pollFDs[playerNumber].fd == -1) {
+            continue;
+        }
+
+        // Send message type
+        int8_t messageType = 7 + phase;
+        sendMessage(&messageType, 1, "message type", playerNumber);
+    }
+}
+
+void receiveMove() {
+    // Check whether move is valid
+    map_isMoveValid();
+
+    // Apply move
+
+    // Print some stuff for map
+
+    // Send move to other players
+    sendMoveAnnoucement();
+}
+
+
+
+//// Public functions
+/////////////////////
+void server_initServer(char *hostParam, char *portParam, int timeLimitParam, int depthLimitParam) {
+    host = hostParam;
+    port = portParam;
+    timeLimit = timeLimitParam;
+    depthLimit = depthLimitParam;
+    connectionLimit = NUM_PLAYERS;
+}
+
+struct pollfd* server_acceptConnections() {
     // Listening socket descriptor
     int listener;
 
@@ -121,14 +256,11 @@ void startServer(char* bindAddress, int port) {
     struct sockaddr_storage remoteAddress;
     socklen_t addressLength;
 
-    // Buffer for client data
-    char buf[256];
-
     char remoteIP[INET6_ADDRSTRLEN];
 
-    // Start off with space for 8 connections
-    int fdCount = 0;
-    int fdSize = 9;
+    // Start off with space for connectionLimit + 1 (the +1 is for the listener socket)
+    fdCount = 0;
+    int fdSize = connectionLimit + 1;
 
     /*
      * struct pollfd {
@@ -137,10 +269,10 @@ void startServer(char* bindAddress, int port) {
      *      short revents;  // when poll() returns, bitmap of events that occurred
      *  };
      */
-    struct pollfd* pollFDs = malloc(sizeof(*pollFDs) * fdSize);
+    pollFDs = malloc(sizeof(*pollFDs) * fdSize);
 
     // Set up and get a listening socket
-    listener = getListenerSocket();
+    listener = getListenerSocket(connectionLimit);
 
     if (listener == -1) {
         fprintf(stderr, RED "Error getting listening socket!" RESET "\n");
@@ -154,8 +286,11 @@ void startServer(char* bindAddress, int port) {
     // For the listener
     fdCount = 1;
 
-    // 1. LET ALL PLAYERS CONNECT TO THE SERVER
-    while (fdCount != 9) {
+    printf("Waiting for " BOLDGREEN "%i" RESET " clients to connect...\n", connectionLimit);
+    fflush(stdout);
+
+    // Let all players connect to the server
+    while (fdCount != fdSize) {
         int pollCount = poll(pollFDs, fdCount, -1);
 
         if (pollCount == -1) {
@@ -163,77 +298,124 @@ void startServer(char* bindAddress, int port) {
             exit(1);
         }
 
-        // Run through the existing connections looking for data to read
-        for(int i = 0; i < fdCount; i++) {
-            // Check if FD is ready to read
-            if (pollFDs[i].revents & POLLIN) {
+        // Check if listener FD is ready to read
+        if (pollFDs[0].revents & POLLIN) {
+            addressLength = sizeof(remoteAddress);
+            newFD = accept(listener, (struct sockaddr*) &remoteAddress, &addressLength);
+            const void* socketAddress = getSocketAddress((struct sockaddr*) &remoteAddress);
 
-                // If listener is ready to read, handle new connection
-                if (pollFDs[i].fd == listener) {
-                    addressLength = sizeof(remoteAddress);
-                    newFD = accept(listener, (struct sockaddr*) &remoteAddress, &addressLength);
-                    const void* socketAddress = getSocketAddress((struct sockaddr*) &remoteAddress);
-
-                    if (newFD == -1) {
-                        fprintf(stderr, RED "Error while accepting connection!" "\n");
-                        fprintf(stderr,
-                                "Remote address:" BLUE "%s\n" RESET,
-                                inet_ntop(remoteAddress.ss_family, socketAddress, remoteIP, INET6_ADDRSTRLEN));
-                    } else {
-                        printf(GREEN "Player %i connected with address " BLUE "%s" RESET,
-                               fdCount,
-                               inet_ntop(remoteAddress.ss_family, socketAddress, remoteIP, INET6_ADDRSTRLEN));
-
-                        // Send map and player number
-
-
-                        addFD(&pollFDs, newFD, &fdCount);
-                    }
-                }
-            } // END got ready-to-read from poll()
-        } // END looping through file descriptors
-    } // END for(;;)--and you thought it would never end!
-}
-
-void handleData() {
-    if (pollFDs[i].fd == listener) {
-
-    } else {
-            // If not the listener, we're just a regular client
-            int nbytes = recv(pollFDs[i].fd, buf, sizeof buf, 0);
-
-            int sender_fd = pollFDs[i].fd;
-
-            if (nbytes <= 0) {
-                // Got error or connection closed by client
-                if (nbytes == 0) {
-                    // Connection closed
-                    printf("pollserver: socket %d hung up\n", sender_fd);
-                } else {
-                    perror("recv");
-                }
-
-                close(pollFDs[i].fd); // Bye!
-
-                del_from_pfds(pollFDs, i, &fdCount);
-
+            if (newFD == -1) {
+                fprintf(stdout, RED "Error while accepting connection!" "\n");
+                fprintf(stdout,
+                        "Remote address:" CYAN "%s\n" RESET,
+                        inet_ntop(remoteAddress.ss_family, socketAddress, remoteIP, INET6_ADDRSTRLEN));
             } else {
-                // We got some good data from a client
+                printf("Player " BLUE "%i" RESET " connected with remote address " CYAN "%s\n" RESET,
+                       fdCount,
+                       inet_ntop(remoteAddress.ss_family, socketAddress, remoteIP, INET6_ADDRSTRLEN));
 
-                for(int j = 0; j < fdCount; j++) {
-                    // Send to everyone!
-                    int dest_fd = pollFDs[j].fd;
+                addFD(newFD);
+            }
 
-                    // Except the listener and ourselves
-                    if (dest_fd != listener && dest_fd != sender_fd) {
-                        if (send(dest_fd, buf, nbytes, 0) == -1) {
-                            perror("send");
-                        }
-                    }
+            fflush(stdout);
+        } else {
+            // If not the listener, then it's a client. We only allow disconnects, otherwise ignore
+            char message[256];
+            for (int i = 1; i < fdSize; ++i) {
+                if (pollFDs[i].revents & POLLIN && recv(pollFDs[i].fd, message, sizeof(message), 0) == 0) {
+                    // Connection closed by client
+                    printf("Player " BLUE "%i" RESET " closed the connection!\n", i);
+                    fflush(stdout);
+                    close(pollFDs[i].fd);
+                    removeFDInConnectionPhase(i);
                 }
             }
         }
+    }
+
+    return pollFDs;
 }
 
-void receiveMove() {
+void server_sendMapData(char* mapString) {
+    // Send map to all connected clients
+    for(int playerNumber = 1; playerNumber <= fdCount; playerNumber++) {
+        if (pollFDs[playerNumber].fd == -1) {
+            continue;
+        }
+
+        // Send message type
+        int8_t messageType = 2;
+        sendMessage(&messageType, 1, "message type", playerNumber);
+
+        // Send length
+        size_t length = strlen(mapString);
+        int32_t mapSize = htonl(length);
+        sendMessage(&mapSize, 4, "message size", playerNumber);
+
+        // Send map
+        sendMessage(mapString, length, "map", playerNumber);
+    }
 }
+
+void server_sendPlayerNumber() {
+    // Send respective player numbers to all connected clients
+    for (int playerNumber = 1; playerNumber <= fdCount; playerNumber++) {
+        if (pollFDs[playerNumber].fd == -1) {
+            continue;
+        }
+
+        // Send message type
+        int8_t messageType = 3;
+        sendMessage(&messageType, 1, "message type", playerNumber);
+
+        // Send length
+        int32_t length = htonl(1);
+        sendMessage(&length, 4, "message size", playerNumber);
+
+        // Send player number
+        int8_t player = playerNumber;
+        sendMessage(&player, 1, "player number", playerNumber);
+    }
+}
+
+void server_startGame() {
+    while (true) {
+        for (int playerNumber = 1; playerNumber <= fdCount; ++playerNumber) {
+            if (pollFDs[playerNumber].fd == -1) {
+                continue;
+            }
+
+            // Send move request to player
+            sendMoveRequest(playerNumber);
+
+            // Wait if time limit is not 0
+            int waitingTime = timeLimit == 0 ? -1 : timeLimit;
+            int pollCount = poll(pollFDs, fdCount, waitingTime);
+            if (pollCount == -1) {
+                fprintf(stderr, RED "Error while waiting for move of player " BLUE "%i" RESET "!\n");
+            } else if (pollCount == 0) {
+                // Player timed out while doing a move -> disqualify
+                sendDisqualification(playerNumber)
+            }
+
+            // Player sent a move
+            if (pollFDs[playerNumber].revents & POLLIN) {
+
+            }
+        }
+    }
+}
+
+void server_cleanUp() {
+    // Close connection with all clients
+    for (int i = 1; i <= fdCount; ++i) {
+        close(pollFDs[i].fd);
+    }
+
+    // Close listener socket
+    close(pollFDs[0].fd);
+
+    free(pollFDs);
+}
+
+#pragma clang diagnostic pop
--- src/network/server.h
+++ src/network/server.h
@@ -7,8 +7,16 @@
 
 #include "../globals.h"
 
-extern struct SinglyLinkedList* headPointer;
+void server_initServer(char *hostParam, char *portParam, int timeLimitParam, int depthLimitParam);
 
-void startServer(char* bindAddress, int port);
+struct pollfd* server_acceptConnections();
+
+void server_sendMapData(char* mapString);
+
+void server_sendPlayerNumber();
+
+void server_startGame();
+
+void server_cleanUp();
 
 #endif //REVERSI_SERVER_SERVER_H
